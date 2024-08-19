91782c0445743c1f3f8bdb9e60030ee20a0ae2ef
adesinna <aladesaea@yahoo.com>
2024-08-18 22:37:23 -0400

    awslbc

--- 2-AWS/1-AWS_DevOps/1-EKS-I/bash.sh
+++ 2-AWS/1-AWS_DevOps/1-EKS-I/bash.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Install AWS CLI
+# Update and install necessary packages
 sudo apt-get update -y
-sudo apt install unzip -y
+sudo apt install unzip curl -y
+
+# Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+rm -rf awscliv2.zip aws
 
 # Configure AWS CLI
 read -p "Enter your AWS Access Key ID: " aws_access_key_id
@@ -16,29 +19,30 @@ aws configure set aws_secret_access_key "$aws_secret_access_key"
 aws configure set default.region us-west-1
 aws configure set default.output_format json
 
-# Install kubectl binary
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/arm64/kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/arm64/kubectl.sha256
-sha256sum -c kubectl.sha256
-openssl sha1 -sha256 kubectl
-chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+# Fetch the latest kubectl version
+KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/arm64/kubectl"
+curl -LO "https://dl.k8s.io/${KUBECTL_VERSION}/bin/linux/arm64/kubectl.sha256"
+echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
+chmod +x kubectl
+mkdir -p $HOME/bin && mv ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
-kubectl version --short --client
+source ~/.bashrc
+
 
 # Install eksctl
 ARCH=arm64
 PLATFORM=$(uname -s)_$ARCH
-
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
 # (Optional) Verify checksum
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-
 sudo mv /tmp/eksctl /usr/local/bin
 
-echo "AWS CLI setup, kubectl and eksctl installation completed."
+echo "AWS CLI setup, kubectl, and eksctl installation completed."
 
 exit
